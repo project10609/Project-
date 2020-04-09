c86ff1ad0f2caedef3bea0a756008b36d9833430
Michael <michael@ip142-64.wlan.ntust.edu.tw>
2020-04-09 11:13:53 +0800

    格式跑掉

--- search/views.py
+++ search/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView,DetailView
 from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
+from products.forms import ProductFilterForm, ProductPriceForm, ProductSourceForm
 # Create your views here.
 
 def search(request):
@@ -15,38 +16,221 @@ def search(request):
         products = Product.objects.filter(product_category__slug__icontains=category).filter(product_name__icontains=query)
         result = Queries.objects.create(search=query,user=request.user)
         result.save()
+        if request.method == 'GET' and 'filter_by' in request.GET or request.method == 'GET' and 'source' in request.GET or request.method == 'GET' and 'min_price' in request.GET or request.method == 'GET' and 'max_price' in request.GET:
+            sortingform = ProductFilterForm(request.GET)
+            sourceform = ProductSourceForm(request.GET)
+            priceform = ProductPriceForm(request.GET)
+            if sortingform.is_valid() or sourceform.is_valid() or priceform.is_valid():
+                min_price = request.GET.get('min_price',None)
+                max_price = request.GET.get('max_price',None)
+                source = request.GET.getlist('source', None)
+                sortby = sortingform.cleaned_data.get('filter_by')
+                if sortby == 'PriceAsc':
+                    if source:
+                        if min_price and max_price:
+                            products = products.filter(product_source__slug__in=source).filter(product_price__range=(min_price,max_price)).order_by('product_price')
+                        else:
+                            products = products.filter(product_source__slug__in=source).order_by('product_price')
+                    else:
+                        if min_price and max_price:
+                            products = products.filter(product_price__range=(min_price,max_price)).order_by('product_price')
+                        else:
+                            products = products.order_by('product_price')
+                elif sortby == 'PriceDesc':
+                    if source:
+                        if min_price and max_price:
+                            products = products.filter(product_source__slug__in=source).filter(product_price__range=(min_price,max_price)).order_by('-product_price')
+                        else:
+                            products = products.filter(product_source__slug__in=source).order_by('-product_price')
+                    else:
+                        if min_price and max_price:
+                            products = products.filter(product_price__range=(min_price,max_price)).order_by('-product_price')
+                        else:
+                            products = products.order_by('-product_price')
+                else:
+                    if source:
+                        if min_price and max_price:
+                            products = products.filter(product_source__slug__in=source).filter(product_price__range=(min_price,max_price)).order_by('?')
+                        else:
+                            products = products.filter(product_source__slug__in=source).order_by('?')
+                    else:
+                        if min_price and max_price:
+                            products = Product.objects.filter(product_category__slug__icontains=category).filter(product_price__range=(min_price,max_price)).order_by('?')
+                        else:
+                            products = products.order_by('?')
+
+            paginator = Paginator(products,20)
+            try:
+                page = request.GET.get('page','1')
+            except:
+                page = 1
+            try:
+                product_list = paginator.page(page)
+            except(EmptyPage, InvalidPage):
+                product_list = paginator.page(1)
+
+            index = product_list.number - 1
+            max_index = len(paginator.page_range)
+            start_index = index - 5 if index >= 5 else 0
+            end_index = index + 5 if index <= max_index - 5 else max_index
+            page_range = list(paginator.page_range)[start_index:end_index]
+
+            total = products.count()
+
+            context = {
+                'product_list':product_list,
+                'total':total,
+                'page_range':page_range,
+                'category':category,
+                'sortingform':sortingform,
+                'sourceform':sourceform,
+                'priceform':priceform,
+            }
+
+            return render(request,'search/search_result.html',context)
+
+        else:
+            sortingform = ProductFilterForm()
+            sourceform = ProductSourceForm()
+            priceform = ProductPriceForm()
+
+            paginator = Paginator(products,20)
+            try:
+                page = request.GET.get('page','1')
+            except:
+                page = 1
+            try:
+                product_list = paginator.page(page)
+            except(EmptyPage, InvalidPage):
+                product_list = paginator.page(1)
+
+            index = product_list.number - 1
+            max_index = len(paginator.page_range)
+            start_index = index - 5 if index >= 5 else 0
+            end_index = index + 5 if index <= max_index - 5 else max_index
+            page_range = list(paginator.page_range)[start_index:end_index]
+
+            context={
+                'products':products,
+                'product_list':product_list,
+                'page_range':page_range,
+                'category':category,
+                'sortingform':sortingform,
+                'sourceform':sourceform,
+                'priceform':priceform,
+            }
+            return render(request,'search/search_result.html',context)
+
     if category == "":
         products = Product.objects.filter(product_name__icontains=query)
         result = Queries.objects.create(search=query,user=request.user)
         result.save()
+        if request.method == 'GET' and 'filter_by' in request.GET or request.method == 'GET' and 'source' in request.GET or request.method == 'GET' and 'min_price' in request.GET or request.method == 'GET' and 'max_price' in request.GET:
+            sortingform = ProductFilterForm(request.GET)
+            sourceform = ProductSourceForm(request.GET)
+            priceform = ProductPriceForm(request.GET)
+            if sortingform.is_valid() or sourceform.is_valid() or priceform.is_valid():
+                min_price = request.GET.get('min_price',None)
+                max_price = request.GET.get('max_price',None)
+                source = request.GET.getlist('source', None)
+                sortby = sortingform.cleaned_data.get('filter_by')
+                if sortby == 'PriceAsc':
+                    if source:
+                        if min_price and max_price:
+                            products = products.filter(product_source__slug__in=source).filter(product_price__range=(min_price,max_price)).order_by('product_price')
+                        else:
+                            products = products.filter(product_source__slug__in=source).order_by('product_price')
+                    else:
+                        if min_price and max_price:
+                            products = products.filter(product_price__range=(min_price,max_price)).order_by('product_price')
+                        else:
+                            products = products.order_by('product_price')
+                elif sortby == 'PriceDesc':
+                    if source:
+                        if min_price and max_price:
+                            products = products.filter(product_source__slug__in=source).filter(product_price__range=(min_price,max_price)).order_by('-product_price')
+                        else:
+                            products = products.filter(product_source__slug__in=source).order_by('-product_price')
+                    else:
+                        if min_price and max_price:
+                            products = products.filter(product_price__range=(min_price,max_price)).order_by('-product_price')
+                        else:
+                            products = products.order_by('-product_price')
+                else:
+                    if source:
+                        if min_price and max_price:
+                            products = products.filter(product_source__slug__in=source).filter(product_price__range=(min_price,max_price)).order_by('?')
+                        else:
+                            products = products.filter(product_source__slug__in=source).order_by('?')
+                    else:
+                        if min_price and max_price:
+                            products = Product.objects.filter(product_category__slug__icontains=category).filter(product_price__range=(min_price,max_price)).order_by('?')
+                        else:
+                            products = products.order_by('?')
+            paginator = Paginator(products,20)
+            try:
+                page = request.GET.get('page','1')
+            except:
+                page = 1
+            try:
+                product_list = paginator.page(page)
+            except(EmptyPage, InvalidPage):
+                product_list = paginator.page(1)
+
+            index = product_list.number - 1
+            max_index = len(paginator.page_range)
+            start_index = index - 5 if index >= 5 else 0
+            end_index = index + 5 if index <= max_index - 5 else max_index
+            page_range = list(paginator.page_range)[start_index:end_index]
+
+            total = products.count()
+
+            context = {
+                'product_list':product_list,
+                'total':total,
+                'page_range':page_range,
+                'category':category,
+                'sortingform':sortingform,
+                'sourceform':sourceform,
+                'priceform':priceform,
+            }
+
+            return render(request,'search/search_result.html',context)
+
+        else:
+            sortingform = ProductFilterForm()
+            sourceform = ProductSourceForm()
+            priceform = ProductPriceForm()
+
+            paginator = Paginator(products,20)
+            try:
+                page = request.GET.get('page','1')
+            except:
+                page = 1
+            try:
+                product_list = paginator.page(page)
+            except(EmptyPage, InvalidPage):
+                product_list = paginator.page(1)
+
+            index = product_list.number - 1
+            max_index = len(paginator.page_range)
+            start_index = index - 5 if index >= 5 else 0
+            end_index = index + 5 if index <= max_index - 5 else max_index
+            page_range = list(paginator.page_range)[start_index:end_index]
+
+            context={
+                'products':products,
+                'product_list':product_list,
+                'page_range':page_range,
+                'category':category,
+                'sortingform':sortingform,
+                'sourceform':sourceform,
+                'priceform':priceform,
+            }
+            return render(request,'search/search_result.html',context)
 
 
-    paginator = Paginator(products,20)
-    try:
-        page = request.GET.get('page','1')
-    except:
-        page = 1
-    try:
-        product_list = paginator.page(page)
-    except(EmptyPage, InvalidPage):
-        product_list = paginator.page(1)
 
-    index = product_list.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 5 if index >= 5 else 0
-    end_index = index + 5 if index <= max_index - 5 else max_index
-    page_range = list(paginator.page_range)[start_index:end_index]
-
-    total = products.count()
-
-    context = {
-        'product_list':product_list,
-        'total':total,
-        'page_range':page_range,
-
-    }
-
-    return render(request,'search/search_result.html',context)
 
 
 
