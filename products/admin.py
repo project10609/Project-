c9bb533d422df47437d1e073770154065ee9f650
Michael <michael@ip142-48.wlan.ntust.edu.tw>
2020-04-10 15:58:27 +0800

    Added category admin

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import *
 
 
+class SubCategoriesAdmin(admin.ModelAdmin):
+    list_display = ('subcategory_name', 'category', 'slug')
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+admin.site.register(Subcategories, SubCategoriesAdmin)
 admin.site.register(Categories)
 admin.site.register(Product)
 admin.site.register(Source)
-admin.site.register(Subcategories)
