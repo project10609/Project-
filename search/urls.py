8d68ac4dd0b492e02d56081ba661d64e821bbb57
Michael <michael@ip142-10.wlan.ntust.edu.tw>
2020-04-12 18:14:47 +0800

    Next changes

--- search/urls.py
+++ search/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from search.views import search
+from search.views import search, search_from_home
 
 
 app_name = 'search'
 
 urlpatterns = [
     path('', search, name='search'),
+    path('<pk>', search_from_home, name='search_from_home'),
 ]
--- search/views.py
+++ search/views.py
@@ -4,10 +4,13 @@ from .models import Queries
 from django.db.models import Q
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
 from django.contrib.auth.models import User
 from products.forms import ProductFilterForm, ProductPriceForm, ProductSourceForm
 from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
+from functools import reduce
+import operator
 # Create your views here.
 
 
@@ -249,6 +252,123 @@ def search(request):
                 'priceform': priceform,
             }
             return render(request, 'search/search_result.html', context)
+
+
+def search_from_home(request, pk):
+    products = Product.objects.all()
+    query = Queries.objects.filter(search=pk).values('search')
+    products = products.filter(
+        reduce(operator.or_, (Q(product_name__icontains=x['search']) for x in query)))
+    if request.method == 'GET' and 'filter_by' in request.GET or request.method == 'GET' and 'source' in request.GET or request.method == 'GET' and 'min_price' in request.GET or request.method == 'GET' and 'max_price' in request.GET:
+        sortingform = ProductFilterForm(request.GET)
+        sourceform = ProductSourceForm(request.GET)
+        priceform = ProductPriceForm(request.GET)
+        if sortingform.is_valid() or sourceform.is_valid() or priceform.is_valid():
+            min_price = request.GET.get('min_price', None)
+            max_price = request.GET.get('max_price', None)
+            source = request.GET.getlist('source', None)
+            sortby = sortingform.cleaned_data.get('filter_by')
+            if sortby == 'PriceAsc':
+                if source:
+                    if min_price and max_price:
+                        products = products.filter(product_source__slug__in=source).filter(
+                            product_price__range=(min_price, max_price)).order_by('product_price')
+                    else:
+                        products = products.filter(
+                            product_source__slug__in=source).order_by('product_price')
+                else:
+                    if min_price and max_price:
+                        products = products.filter(product_price__range=(
+                            min_price, max_price)).order_by('product_price')
+                    else:
+                        products = products.order_by('product_price')
+            elif sortby == 'PriceDesc':
+                if source:
+                    if min_price and max_price:
+                        products = products.filter(product_source__slug__in=source).filter(
+                            product_price__range=(min_price, max_price)).order_by('-product_price')
+                    else:
+                        products = products.filter(
+                            product_source__slug__in=source).order_by('-product_price')
+                else:
+                    if min_price and max_price:
+                        products = products.filter(product_price__range=(
+                            min_price, max_price)).order_by('-product_price')
+                    else:
+                        products = products.order_by('-product_price')
+            else:
+                if source:
+                    if min_price and max_price:
+                        products = products.filter(product_source__slug__in=source).filter(
+                            product_price__range=(min_price, max_price)).order_by('?')
+                    else:
+                        products = products.filter(
+                            product_source__slug__in=source).order_by('?')
+                else:
+                    if min_price and max_price:
+                        products = Product.objects.filter(product_category__slug__icontains=category).filter(
+                            product_price__range=(min_price, max_price)).order_by('?')
+                    else:
+                        products = products.order_by('?')
+
+        paginator = Paginator(products, 20)
+        try:
+            page = request.GET.get('page', '1')
+        except:
+            page = 1
+        try:
+            product_list = paginator.page(page)
+        except(EmptyPage, InvalidPage):
+            product_list = paginator.page(1)
+
+        index = product_list.number - 1
+        max_index = len(paginator.page_range)
+        start_index = index - 5 if index >= 5 else 0
+        end_index = index + 5 if index <= max_index - 5 else max_index
+        page_range = list(paginator.page_range)[start_index:end_index]
+
+        context = {
+            'product_list': product_list,
+
+            'page_range': page_range,
+            'category': category,
+            'sortingform': sortingform,
+            'sourceform': sourceform,
+            'priceform': priceform,
+        }
+
+        return render(request, 'search/search_result.html', context)
+
+    else:
+        sortingform = ProductFilterForm()
+        sourceform = ProductSourceForm()
+        priceform = ProductPriceForm()
+
+        paginator = Paginator(products, 20)
+        try:
+            page = request.GET.get('page', '1')
+        except:
+            page = 1
+        try:
+            product_list = paginator.page(page)
+        except(EmptyPage, InvalidPage):
+            product_list = paginator.page(1)
+
+        index = product_list.number - 1
+        max_index = len(paginator.page_range)
+        start_index = index - 5 if index >= 5 else 0
+        end_index = index + 5 if index <= max_index - 5 else max_index
+        page_range = list(paginator.page_range)[start_index:end_index]
+
+        context = {
+            'products': products,
+            'product_list': product_list,
+            'page_range': page_range,
+            'sortingform': sortingform,
+            'sourceform': sourceform,
+            'priceform': priceform,
+        }
+        return render(request, 'search/search_result.html', context)
 
 
 # class SearchListView(ListView):
