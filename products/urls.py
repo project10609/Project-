0e27eecf92ead6d801f1049be24dfa0ae016e4da
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-04-19 14:09:38 +0800

    ADDED 'Productlist.html' to show all Product List

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, re_path
-from products.views import category_item, subcategory
+from products.views import category_item, subcategory, product_list
 from Rating.views import product_detail
 
 app_name = 'products'
 
 urlpatterns = [
+    path('', product_list, name='allproduct'),
     path('<int:pk>/', product_detail, name='product_detail'),
     path('category/<pk>/', category_item, name='categories'),
     path('subcategory/<pk>', subcategory, name='subcategory')
+
     # path('category/<str:category>/<str:source>',source_sorting,name='source_sorting'),
 ]
