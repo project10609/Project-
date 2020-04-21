829c0ae7e743e9aaa7396c552261fac058c81bee
project10609 <project10609@gmail.com>
2020-04-21 21:56:21 +0800

    更新2020/4/21 09:56 pm

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from products.views import category_item, subcategory, product_list
-from Rating.views import product_detail, delete_rating, update_rating
+from Rating.views import product_detail, delete_rating
 
 
 app_name = 'products'
@@ -12,8 +12,6 @@ urlpatterns = [
     path('category/<pk>/', category_item, name='categories'),
     path('subcategory/<pk>/', subcategory, name='subcategory'),
     path('delete/<pk>/<id>/', delete_rating, name="delete_rating"),
-    path('<int:pk>/<int:id>/update/',
-         update_rating, name="update_rating"),
 
 
     # path('category/<str:category>/<str:source>',source_sorting,name='source_sorting'),
