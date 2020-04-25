afcafb9a2215838b7f9f048a8ee248abdb8657c3
michaelzz40 <61964609+michaelzz40@users.noreply.github.com>
2020-04-25 23:27:48 +0800

    12

--- static/static_root/js/main7.js
+++ static/static_root/js/main7.js
@@ -201,9 +201,9 @@
   /*-------------------
 		Range Slider
 	--------------------- */
-  var rangeSlider = (".price-range"),
-    minamount = ("#minamount"),
-    maxamount = ("#maxamount"),
+  var rangeSlider = $(".price-range"),
+    minamount = $("#minamount"),
+    maxamount = $("#maxamount"),
     minPrice = rangeSlider.data("min"),
     maxPrice = rangeSlider.data("max");
   rangeSlider.slider({
@@ -212,8 +212,8 @@
     max: maxPrice,
     values: [minPrice, maxPrice],
     slide: function (event, ui) {
-      minamount.val(ui.values[0]);
-      maxamount.val(ui.values[1]);
+      minamount.val("$" + ui.values[0]);
+      maxamount.val("$" + ui.values[1]);
     },
   });
   minamount.val(rangeSlider.slider("values", 0));
